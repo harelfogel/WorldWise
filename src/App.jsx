d038e061b2ef25d3f36428011d3d684879782f7f
harelfogel <fogell06@gmail.com>
2024-02-17 14:23:03 +0200

    feature complete

--- src/App.jsx
+++ src/App.jsx
@@ -12,28 +12,36 @@ import Form from './components/Form'
 import { CitiesProvider } from './contexts/CitiesContext';
 //import PageNotFound from './pages/pageNotFound';
 //import AppLayout from './pages/AppLayout';
+import { AuthProvider } from './contexts/FakeAuthCOntext';
+import ProtectedRoute from './pages/ProtectedRoute';
 
 const url = 'http://localhost:9000';
 
 export default function App() {
   return (
-    <CitiesProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route index path="/" element={<Homepage />} />
-          <Route path="product" element={<Product />} />
-          <Route path="pricing" element={<Pricing />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="app" element={<AppLayout />}>
-            <Route index element={<Navigate replace to="countries" />} />
-            <Route path="cities" element={<CityList />} />
-            <Route path='cities/:id' element={<City />} />
-            <Route path="countries" element={<CountryList />} />
-            <Route path="form" element={<Form />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </CitiesProvider>
+    <AuthProvider>
+      <CitiesProvider>
+        <BrowserRouter>
+          <Routes>
+            <Route index path="/" element={<Homepage />} />
+            <Route path="product" element={<Product />} />
+            <Route path="pricing" element={<Pricing />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="app" element={
+              <ProtectedRoute>
+                <AppLayout />
+              </ProtectedRoute>
+            }>
+              <Route index element={<Navigate replace to="countries" />} />
+              <Route path="cities" element={<CityList />} />
+              <Route path='cities/:id' element={<City />} />
+              <Route path="countries" element={<CountryList />} />
+              <Route path="form" element={<Form />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </CitiesProvider>
+    </AuthProvider >
   )
 }
 
--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Sidebar from '../components/Sidebar';
 import Map from '../components/Map';
-
+import User from '../components/User';
 import styles from './AppLayout.module.css';
 
 export default function AppLayout() {
   return (
     <div className={styles.app}>
-        <Sidebar />
-        <Map />
+      <Sidebar />
+      <Map />
+      <User />
     </div>
   )
 }
